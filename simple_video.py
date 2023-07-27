c1184855c4bab1fc83637fba3f91777dbe735c54
RomanGDB <140636406+RomanGDB@users.noreply.github.com>
2023-07-27 15:52:17 -0300

    Add files via upload
    Actualizado los progaramas para comandar la camara.
    Ahora andan con simple-pyspin, una librería que usa PySpin pero que es MUCHO mas facil de usar. Queda mejorar los argumentos para llamar a ambas funciones.

---
+++ simple_video.py
@@ -0,0 +1,77 @@
+import PySpin
+import sys
+import numpy as np
+import keyboard
+import os
+import cv2
+from tools import stokeslib
+from PIL import Image 
+from simple_pyspin import Camera
+
+hsize = 350
+vsize = 250
+hoffset = 40
+voffset = 20
+prop = 0.75
+
+def begin_stream(exposure_time, N):
+
+    #Ventana
+    cv2.namedWindow('img1', cv2.WINDOW_NORMAL)
+    cv2.moveWindow('img1', hoffset, voffset)
+    cv2.resizeWindow('img1', int(prop*int(1.25*hsize)), int(prop*int(1.25*vsize)))
+    cv2.setWindowTitle('img1', 'S0')	
+    
+    #Continua actualizando
+    update = True
+    		
+    with Camera() as cam: # Acquire and initialize Camera
+    	
+        while update:    
+            
+            #Exposicion
+            cam.ExposureAuto = 'Off'
+            cam.ExposureTime = exposure_time # microseconds
+            
+            #Toma las fotos
+            cam.start() # Start recording
+            imgs = [cam.get_array() for n in range(N)] # Get 10 frames
+            cam.stop() # Stop recording
+        
+            #Promedia las fotos
+            img_mean = 1/N*(sum(imgs)).astype(float)
+            
+            #Decodifica la imagen
+            I90, I45, I135, I0 = stokeslib.polarization_full_dec_array(img_mean)
+        
+            # Actualiza imagen
+            cv2.imshow("img1", I90)
+            
+            #Espera comando
+            k = cv2.waitKey(1)
+            
+            if k == ord("q"):
+                cv2.destroyAllWindows()
+                update = False
+        
+    return None
+    
+def main():
+
+    #Exposicion
+    exposure_time = 5000
+    
+    # Numero de promedios
+    N = 1
+   
+    #Toma el video
+    begin_stream(exposure_time, N)
+    
+    return True
+
+if __name__ == '__main__':
+
+    if main():
+        sys.exit(0)
+    else:
+        sys.exit(1)
