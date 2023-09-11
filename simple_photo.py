4a3ff905ae8deaaab5d2dce9b722ec2d807c2eb8
MWSI <dsilvapiedra@gmail.com>
2023-09-11 13:31:05 -0300

    Mejor modulado, stokes por color, barra corregida

--- simple_photo.py
+++ simple_photo.py
@@ -7,13 +7,33 @@ import cv2
 from tools import stokeslib
 from PIL import Image 
 from simple_pyspin import Camera
-from tools.camaralib import take_photo
+from tools.camaralib import take_stokes
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 
 IMG_SAVE_PATH = 'img/'              
     
+def guardar_img(img, name, color = 'white', clim = None):
+    # Magnificacion
+    Mag = 22
+
+    # Guarda figura
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    im = ax.imshow(img, cmap='gray')
+    ax.set_title(name,fontsize = 20)
+    scalebar = AnchoredSizeBar(ax.transData, 0.5*Mag*50/3.45 , '50 μm', 'lower right', pad=0.2, 
+                                color=color, frameon=False, size_vertical=6,
+                                fontproperties=fm.FontProperties(size=14))
+    ax.add_artist(scalebar)
+    if clim != None:
+        cbar = plt.colorbar(im)
+        im.set_clim(vmin=clim[0],vmax=clim[1])
+    plt.savefig(IMG_SAVE_PATH + name + ".png")
+    plt.close()
+    return None
+
 def main(name = None):
 
     #Nombre archivo
@@ -26,41 +46,27 @@ def main(name = None):
     # Numero de promedios
     N = 1
 
-    # Magnificacion
-    Mag = 22
-
-    # Dimension sensor
-    dim = (2048,2448)      
-
-    # Vector de Stokes
-    S = np.zeros((dim[0]//2,dim[1]//2,3,3), dtype=float)
-   
-    #Toma la foto
-    image_data = take_photo(exposure_time, N)
-    
-    # Decodifica
-    I90, I45, I135, I0 = stokeslib.polarization_full_dec_array(image_data)
-    
-    # Calcula Stokes
-    S[:,:,:,0], S[:,:,:,1], S[:,:,:,2] = stokeslib.calcular_stokes(I90, I45, I135, I0)
+    #Capturar Stokes
+    S = take_stokes(exposure_time, N)
        
-    # Crea objeto 
-    im = cv2.cvtColor(S[:,:,:,0].astype(np.uint8),cv2.COLOR_BGR2RGB)
-  
-    # Guarda imagen
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    im = ax.imshow(im)
-    ax.set_title(name,fontsize = 20)
-    scalebar = AnchoredSizeBar(ax.transData, Mag*50/3.45 , '50 μm', 'lower right', pad=0.2, 
-                                color='white', frameon=False, size_vertical=1,
-                                fontproperties=fm.FontProperties(size=12))
-    ax.add_artist(scalebar)
-    plt.savefig(IMG_SAVE_PATH + name + ".png")
+    # Guarda imagenes
+    color = ['450','550','650']
+    
+    #S0 en color
+    im_S0 = cv2.cvtColor(S[:,:,:,0].astype(np.uint8),cv2.COLOR_BGR2RGB)
+    guardar_img(im_S0, name + ' S0')
+    S0_max = np.max(S[:,:,0,0])
 
+    #S0 S1 y S2 en grises con colorbar
+    for i in range(3):
+        for j in range(3):
+            im = cv2.cvtColor(cv2.normalize(S[:,:,i,j], None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1),cv2.COLOR_BGR2RGB)
+            lim = [np.min(S[:,:,i,j])/S0_max,np.max(S[:,:,i,j])/S0_max]
+            guardar_img(im, name + ' S' + str(j) + ' ('+color[i]+' nm)', color = 'red', clim=lim)
+    
     #Guarda Array
-    with open(IMG_SAVE_PATH + name + ".npy", 'wb') as f:
-        np.save(f, S)
+    #with open(IMG_SAVE_PATH + name + ".npy", 'wb') as f:
+    #    np.save(f, S)
 
     return True
 
